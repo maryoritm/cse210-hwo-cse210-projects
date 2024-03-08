1e73f81d10ef6b90872fb91b42d573ec1b1cd088
maryoritm <maryor.trrs@gmail.com>
2024-03-08 17:13:05 -0600

    activity

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
+
+        Console.WriteLine("Hello learning02 world");
+    }
+}
